7c2d6cdec0b3c124025e880d67ed753225c274c1
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-08-10 10:06:21 -0700

    localize, closes #9

--- lifterlms-lite-lms.php
+++ lifterlms-lite-lms.php
@@ -4,13 +4,13 @@
  *
  * @package LifterLMS_Lite_LMS/Main
  *
- * @since 0.0.1
- * @version 0.0.1
+ * @since [version]
+ * @version [version]
  *
  * Plugin Name: Lite LMS by LifterLMS
  * Plugin URI: https://lifterlms.com/
  * Description: Easily track progress through simple online courses and other types of public or membership-protected content on your WordPress website.
- * Version: 0.0.1
+ * Version: [version]
  * Author: LifterLMS
  * Author URI: https://lifterlms.com/
  * Text Domain: lifterlms-lite-lms
@@ -43,15 +43,29 @@ defined( 'ABSPATH' ) || exit;
  * @return void
  */
 function llms_lite_lms() {
+	llms_lite_lms_scripts();
+	llms_lite_lms_i18n();
+}
+add_action( 'init', 'llms_lite_lms' );
 
-	$dir  = dirname( __FILE__ );
-	$url  = plugin_dir_url( __FILE__ );
-	$slug = is_admin() ? 'editor' : 'client';
+/**
+ * Register & Enqueue scripts.
+ *
+ * @since [version]
+ *
+ * @return void
+ */
+function llms_lite_lms_scripts() {
+
+	$dir    = dirname( __FILE__ );
+	$url    = plugin_dir_url( __FILE__ );
+	$slug   = is_admin() ? 'editor' : 'client';
+	$handle = sprintf( 'llms-lite-%s', $slug );
 
 	$asset = include $dir . '/assets/js/llms-lite-' . $slug . '.asset.php';
 
 	wp_enqueue_script(
-		'llms-lite-editor',
+		$handle,
 		$url . 'assets/js/llms-lite-' . $slug . '.js',
 		$asset['dependencies'],
 		$asset['version'],
@@ -65,5 +79,26 @@ function llms_lite_lms() {
 		$asset['version']
 	);
 
+	// JS translations.
+	if ( 'editor' === $slug ) {
+		wp_set_script_translations( $handle, 'lifterlms-lite-lms' );
+	}
+
 }
-add_action( 'init', 'llms_lite_lms' );
+
+/**
+ * Load textdomain
+ *
+ * @since [version]
+ *
+ * @return void
+ */
+function llms_lite_lms_i18n() {
+
+	$locale = apply_filters( 'plugin_locale', determine_locale(), 'lifterlms' );
+
+	unload_textdomain( 'lifterlms-lite-lms' );
+	load_textdomain( 'lifterlms-lite-lms', WP_LANG_DIR . '/lifterlms/lifterlms-' . $locale . '.mo' );
+	load_plugin_textdomain( 'lifterlms', false, dirname( plugin_basename( __FILE__ ) ) . '/i18n' );
+
+}
