165ee03140d4dd515f9b63ee978435592532235e
Jason Coleman <33220397+ideadude@users.noreply.github.com>
2024-01-15 12:28:26 -0500

    Update tested up to version

--- lifterlms-lite-lms-progress-tracker.php
+++ lifterlms-lite-lms-progress-tracker.php
@@ -18,7 +18,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Requires at least: 5.4
- * Tested up to: 6.1
+ * Tested up to: 6.4.2
  *
  * * * * * * * * * * * * * * * * * * * * * *
  *                                         *
